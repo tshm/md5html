e62743da9a9ab76b86d0527b74c35a10536a5dc1
Tosh Satake <2sm@csc.jp>
2015-11-30 13:07:46 +0900

    define port for file selection

--- main.elm
+++ main.elm
@@ -3,42 +3,93 @@ module Md5html (main) where
 -}
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Signal exposing (Signal, Address)
+import Json.Decode as Json
+import Debug exposing (..)
 
 type alias Model =
   { files : List File
   }
 
-type alias File = String
+type alias File =
+  { name : String
+  , md5 : String
+  }
 
 initModel : Model
 initModel =
-  { files = ["asdf", "file"]
+  { files =
+    [ { name = "test", md5 = "AAA" }
+    , { name = "xxxx", md5 = "999" }
+    ]
   }
 
 type Action
   = NoOp
+  | AddFile String String
 
 update : Action -> Model -> Model
 update action model =
   case action of
     NoOp -> model
+    AddFile name md5 -> { model | files = (File name md5) :: model.files }
 
 view : Address Action -> Model -> Html
 view address model =
-  div
-    [
-    ]
-    <| List.map (\f -> text f) model.files
+  let
+    title = h2 [] [text "title"]
+    list = List.map formatRow model.files
+    formatRow file =
+      tr []
+        [ td []
+             [ text file.name ]
+        , td []
+             [ input
+               [ value file.md5
+               , readonly True
+               ] []
+             ]
+        ]
+  in
+    section
+      []
+      [ title
+      , input [ id "ff", type' "file", on "input" targetValue (\_ -> Signal.message address NoOp)] []
+      , table [] list
+      ]
 
+main = Signal.map (view userActions.address) model
 
-main = Signal.map (view actions.address) model
-
-actions : Signal.Mailbox Action
-actions =
+userActions : Signal.Mailbox Action
+userActions =
   Signal.mailbox NoOp
+
+actions : Signal Action
+actions =
+  Signal.merge userActions.signal addFileAction
 
 model : Signal Model
 model =
-  Signal.foldp update initModel actions.signal
+  Signal.foldp update initModel actions
+
+--onInput : Signal.Address a -> (String -> a) -> Attribute
+onInput address contentToValue =
+  let
+    aa str = 
+      let 
+          x = Debug.watch "x: " str
+      in Signal.message address NoOp
+    --aa str = Signal.message address (contentToValue <| Debug.watch "x:" str)
+  in
+    on "input" targetValue aa
+
+{-| ports
+-}
+port addFile : Signal (String, String)
+
+addFileAction : Signal Action
+addFileAction =
+  Signal.map (\(x, y) -> AddFile x y) addFile
 
