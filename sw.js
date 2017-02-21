1ca01bd55a20c4d9a05f4994926a4020ae03afc5
Tosh Satake <2sm@csc.jp>
2017-02-21 09:17:37 -0800

    add manifest

---
+++ sw.js
@@ -0,0 +1,27 @@
+const PRECACHE = 'v1'
+const RUNTIME = 'runtime'
+
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  'src/main.css',
+  'src/bower_components/jshashes/hashes.min.js',
+  'bundle.js',
+]
+
+self.addEventListener('install', ev => {
+  ev.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  )
+})
+
+self.addEventListener('fetch', ev => {
+  ev.respondWith(
+    caches.match(ev.request).then( response =>
+      response || fetch(ev.request);
+    )
+  )
+})
+
