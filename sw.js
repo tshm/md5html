34744ce0e42d26dd90679da84b6c723db38b0632
Tosh Satake <2sm@csc.jp>
2017-03-14 16:37:12 +0900

    cleanup

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
+/* global self, caches, fetch */
 const PRECACHE = 'v1'
-const RUNTIME = 'runtime'
 
 const PRECACHE_URLS = [
   'index.html',
   './', // Alias for index.html
   'src/main.css',
   'src/bower_components/jshashes/hashes.min.js',
-  'bundle.js',
+  'bundle.js'
 ]
 
 self.addEventListener('install', ev => {
@@ -19,7 +19,7 @@ self.addEventListener('install', ev => {
 
 self.addEventListener('fetch', ev => {
   ev.respondWith(
-    caches.match(ev.request).then( response =>
+    caches.match(ev.request).then(response =>
       response || fetch(ev.request)
     )
   )
