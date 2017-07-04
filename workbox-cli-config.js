1057751939f366f1ad029bc78fb6cd00deeadd05
Tosh Satake <2sm@csc.jp>
2017-07-04 09:31:51 +0900

    remove absolute path indicator

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -2,6 +2,7 @@ module.exports = {
   "globDirectory": "./dist",
   "globPatterns": ["*.*"],
   "swDest": "./dist/sw.js",
+  "modifyUrlPrefix":{"/":""},
   "globIgnores": [
     "workbox-cli-config.js"
   ]
