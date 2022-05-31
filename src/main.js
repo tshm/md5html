682c0cd51d88ef8628d4593f642e345dbcfd9f18
Tosh Satake <satake@empathy.co.jp>
2022-05-31 11:48:18 +0900

    refactor: cleanup

--- src/main.js
+++ src/main.js
@@ -1,39 +1,39 @@
-/* global Worker */
-import { Elm } from "./Main.elm";
+import { Elm } from './Main.elm';
+
 const app = Elm.Main.init({
-  node: document.getElementById("elm"),
+  node: document.getElementById('elm'),
 });
-const worker = new Worker(new URL("./worker.js", import.meta.url), {
-  type: "module",
+const worker = new Worker(new URL('./worker.js', import.meta.url), {
+  type: 'module',
 });
 worker.postMessage(false); // load script
-const DEBUG = process.env.NODE_ENV === "development";
+const DEBUG = process.env.NODE_ENV === 'development';
 
-worker.onmessage = function (obj) {
-  const data = obj.data;
+worker.onmessage = function onmessage(obj) {
+  const { data } = obj;
   app.ports.updatefile.send(data);
 };
 
-app.ports.clearFiles.subscribe(function () {
-  const elem = document.getElementById("fileopener");
+app.ports.clearFiles.subscribe(() => {
+  const elem = document.getElementById('fileopener');
   elem.value = null;
 });
 
-app.ports.openFiles.subscribe(function (arg) {
-  console.time("openFiles() called");
+app.ports.openFiles.subscribe((arg) => {
+  console.time('openFiles() called');
   const arrFiles = [].slice.call(arg.files);
-  if (DEBUG) console.log("algoname", arg.algoname);
-  if (DEBUG) console.log("file(s) added:", arrFiles);
+  if (DEBUG) console.log('algoname', arg.algoname);
+  if (DEBUG) console.log('file(s) added:', arrFiles);
 
-  arrFiles.forEach(function (file) {
-    console.log("sending", file.name);
+  arrFiles.forEach((file) => {
+    console.log('sending', file.name);
     app.ports.addfile.send(file.name);
     worker.postMessage({
       algoname: arg.algoname,
-      file: file,
+      file,
     });
   });
-  console.timeEnd("openFiles() called");
+  console.timeEnd('openFiles() called');
 });
 
-console.log("loaded...");
+console.log('loaded...');
--- src/worker.js
+++ src/worker.js
@@ -1,40 +1,44 @@
-/* global self */
-const CryptoJS = require('crypto-js')
-
-self.onmessage = function(e) {
-  if (!e.data) return
-  const reader = new self.FileReader()
-  const file = e.data.file
-
-  reader.onload = function(ev) {
-    const array = arrayBufferToWordArray(ev.target.result)
-    const hash = CryptoJS[e.data.algoname](array)
-    self.postMessage({
-      name: file.name,
-      hash: hash.toString()
-    })
-  }
-
-  reader.onerror = function(e) {
-    self.postMessage({
-      file: file.name,
-      hash: 'file read error...'
-    })
-  }
-
-  self.postMessage({
-    name: file.name,
-    hash: 'start processing...'
-  })
-  reader.readAsArrayBuffer(file)
-}
+/* eslint-disable no-bitwise */
+const CryptoJS = require('crypto-js');
 
 function arrayBufferToWordArray(arrayBuf) {
-  const intArr = new Uint8Array(arrayBuf)
-  const wordArr = []
+  const intArr = new Uint8Array(arrayBuf);
+  const wordArr = [];
   for (let i = 0; i < intArr.length; i += 4) {
-    const v = intArr[i] << 24 | intArr[i + 1] << 16 | intArr[i + 2] << 8 | intArr[i + 3]
-    wordArr.push(v)
+    const v =
+      (intArr[i] << 24) |
+      (intArr[i + 1] << 16) |
+      (intArr[i + 2] << 8) |
+      intArr[i + 3];
+    wordArr.push(v);
   }
-  return CryptoJS.lib.WordArray.create(wordArr, intArr.length)
+  return CryptoJS.lib.WordArray.create(wordArr, intArr.length);
 }
+
+onmessage = function onmessage(e) {
+  if (!e.data) return;
+  const reader = new FileReader();
+  const { file } = e.data;
+
+  reader.onload = function onload(ev) {
+    const array = arrayBufferToWordArray(ev.target.result);
+    const hash = CryptoJS[e.data.algoname](array);
+    postMessage({
+      name: file.name,
+      hash: hash.toString(),
+    });
+  };
+
+  reader.onerror = function onerror() {
+    postMessage({
+      file: file.name,
+      hash: 'file read error...',
+    });
+  };
+
+  postMessage({
+    name: file.name,
+    hash: 'start processing...',
+  });
+  reader.readAsArrayBuffer(file);
+};
